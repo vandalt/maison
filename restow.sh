a39031aac8b1a6c0099ff7ec73291c4cc16abc4c
Thomas Vandal <thomasvandal@hotmail.com>
2022-01-06 07:58:38 -0500

    add new dirs to stow

--- restow.sh
+++ restow.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-stow -vRt ~ shell sway waybar mako defaults alacritty kitty applications libinput-gestures nvim
+stow -vRt ~ shell sway waybar mako defaults alacritty kitty applications libinput-gestures nvim swaylock kanshi tridactyl gammastep ipython
