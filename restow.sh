444688b728f7411f5761c2de531b5bb2375624e0
Thomas Vandal <thomasvandal@hotmail.com>
2022-04-09 16:46:36 -0400

    Updateed stow

--- restow.sh
+++ restow.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
-stow -vRt ~ shell bin applications nvim tridactyl ipython task neomutt systemd qutebrowser urlview chromium tmux libinput-gestures jupyterlab
+# Generic
+stow -vRt ~ shell bin applications nvim tridactyl ipython task neomutt systemd qutebrowser urlview chromium tmux libinput-gestures jupyterlab zathura flake8
 
 # For sway
-#stow -vRt ~ shell bin sway waybar mako defaults alacritty kitty applications libinput-gestures nvim swaylock kanshi tridactyl gammastep ipython task neomutt systemd qutebrowser wofi urlview
+stow -vRt ~ sway waybar mako defaults alacritty kitty libinput-gestures swaylock kanshi gammastep wofi
 
 cd garage
 
-stow -vRt ~ neomutt rbw newsboat pim git task zoom
+stow -vRt ~ neomutt rbw newsboat pim git zoom onedrive
 
 cd ..
