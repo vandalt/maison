b88c487a3b196e32eb3087f83644e2b781938433
Thomas Vandal <thomasvandal@hotmail.com>
2022-04-09 08:45:40 -0400

    Update IPython vi keys

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -1,1 +1,10 @@
+import warnings
+
 c.TerminalInteractiveShell.editing_mode = 'vi'
+
+c.TerminalIPythonApp.log_level = "ERROR"
+try:
+    c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
+    c.TerminalInteractiveShell.timeoutlen = 0.25
+except:
+    pass
