5d4f85567df45ff332ebca87294010095a703d0d
Thomas Vandal <thomasvandal@hotmail.com>
2023-05-19 12:58:29 +0200

    Simpler restow

--- restow.sh
+++ restow.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/bash
 
-# Generic
-#stow -vRt ~ shell bin applications nvim tridactyl ipython neomutt task systemd qutebrowser urlview chromium tmux libinput-gestures jupyterlab zathura flake8
-
-stow -vRt ~ shell ipython nvim bin tridactyl applications systemd task qutebrowser urlview tmux libinput-gestures jupyterlab zathura flake8 neomutt matplotlib kitty
-
-# For sway
-#stow -vRt ~ sway waybar mako defaults alacritty kitty libinput-gestures swaylock kanshi gammastep wofi
-stow -vRt ~ kanshi mako waybar swaylock gammastep libinput-gestures sway defaults
+stow -vRt --no-folding ~ nvim bin applications shell kitty task matplotlib neomutt
 
 cd garage || exit
 
-stow -vRt ~ neomutt rbw newsboat pim git zoom onedrive spotifyd
+stow -vRt ~ git onedrive neomutt bin vandalfiles
 
 cd ..
