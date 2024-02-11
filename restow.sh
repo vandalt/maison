6e19a2bf0447ef14e6db83f17d4bba1ae1d0c94c
Thomas Vandal <thomas.vandal@umontreal.ca>
2024-02-11 11:40:35 -0500

    Update restow

--- restow.sh
+++ restow.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 # stow -vRt --no-folding ~ nvim bin applications shell kitty task matplotlib neomutt
-stow --no-folding -vRt ~ bin bash tridactyl kitty zsh
+stow -vRt ~ nvim
+stow --no-folding -vRt ~ bin bash tridactyl sway wezterm kitty zsh gammastep waybar qutebrowser lazygit swaylock kanshi libinput-gestures neomutt systemd ipython mako matplotlib zathura nautilus kmonad hypr
 
 cd garage || exit
 
 # stow -vRt ~ git onedrive neomutt bin vandalfiles
-stow --no-folding -vRt ~ git bin vandalfiles
+stow --no-folding -vRt ~ git bin vandalfiles neomutt pim onedrive paru taskwarrior 
 
 cd ..
